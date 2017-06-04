78eeeccf971674c671068ba750beb584e8e761ae
y.okudera <mail@sample.com>
2017-06-04 18:20:41 +0900

    [Add] 画像タップで画面の明るさを変更するサンプルを追加

--- ios-uikit-uiscreen-demo/ViewController.swift
+++ ios-uikit-uiscreen-demo/ViewController.swift
@@ -9,10 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var lightButton: UIButton!
 
+    // MARK: - view life cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         renderScreenSize()
+        
+        let brightnessValue = UIScreen.main.brightness
+        print("変更前の画面輝度: \(brightnessValue)")
     }
     
     /// 画面サイズを表示する
@@ -20,6 +26,18 @@ class ViewController: UIViewController {
         print(UIScreen.screenSize())
         print("高さ: \(UIScreen.screenHeight())")
         print("幅: \(UIScreen.screenWidth())")
+    }
+    
+    // MARK: - action
+    
+    @IBAction func didTapLightButton(_ sender: Any) {
+        
+        if lightButton.isSelected {
+            UIScreen.main.brightness = 0.3
+        } else {
+            UIScreen.main.brightness = 0.6
+        }
+        lightButton.isSelected = !lightButton.isSelected
     }
 }
 
